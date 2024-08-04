ff7631f73d2f217bdab647e2dc6eec744584ec7f
Poghosyan <poghosyan2014@bk.ru>
2024-08-04 22:05:45 +0400

    change content write_to_mongodb.py file

--- python_mongodb_project/write_to_mongodb.py
+++ python_mongodb_project/write_to_mongodb.py
@@ -5,7 +5,14 @@ client = MongoClient("mongodb://mongodb:27017/")
 db = client.my_database
 collection = db.my_collection
 
-document = {"message": "Hello, Sergey when do you come Ashotsk"}
-insert_result = collection.insert_one(document)
-print("Pyhon container exited")
+document = collection.find_one({"_id": "counter"})
+if document is None:
+    collection.insert_one({"_id": "counter", "number": 1})
+    next_number = 1
+else:
+    result = collection.update_one({"_id": "counter"}, {"$inc": {"number": 1}})
+    next_number = document["number"] + 1
+
+print("Python container exited")
+
 client.close()
